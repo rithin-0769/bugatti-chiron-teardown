0739c0dc349df211c0d661bc555ad2e5c2d410e3
Rithin Ravoori <ravooririthin7@gmail.com>
2026-04-06 16:06:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import Lenis from "lenis";
+import {
+  HeroSection,
+  FeaturesSection,
+  CTASection,
+  FooterSection,
+  GlobalResponsive,
+} from "./sections";
+
+export default function App() {
+  // Lenis smooth scroll
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.4,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      wheelMultiplier: 0.8,
+      touchMultiplier: 1.4,
+      overscroll: false,
+    });
+
+    let raf;
+    const tick = (t) => { lenis.raf(t); raf = requestAnimationFrame(tick); };
+    raf = requestAnimationFrame(tick);
+    return () => { cancelAnimationFrame(raf); lenis.destroy(); };
+  }, []);
+
+  return (
+    <>
+      <GlobalResponsive />
+      <HeroSection />
+      <FeaturesSection />
+      <CTASection />
+      <FooterSection />
+    </>
+  );
+}
